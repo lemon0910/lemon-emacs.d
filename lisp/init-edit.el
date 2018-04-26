6258a67cea1037aa55008a24c83e90bd68a68ef9
lemon0910 <lemon9010@gmail.com>
2018-04-26 10:56:43 +0800

    大于指定行数不显示行号

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -79,6 +79,14 @@
   :diminish undo-tree-mode
   :init (add-hook 'after-init-hook #'global-undo-tree-mode))
 
+(defun buffer-too-big-p ()
+  (or (> (buffer-size) (* 5000 80))
+      (> (line-number-at-pos (point-max)) 5000)))
+(add-hook 'prog-mode-hook
+          (lambda ()
+            ;; turn off `linum-mode' when there are more than 5000 lines
+            (if (buffer-too-big-p) (linum-mode -1))))
+
 (provide 'init-edit)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
