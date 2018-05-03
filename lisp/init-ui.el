5b29ff2c36ed59285a88c5cd5d4463952d2659ee
lemon0910 <lemon9010@gmail.com>
2018-05-03 09:08:02 +0800

    增加telephone-line

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -39,6 +39,21 @@
   (use-package leuven-theme
     :init (load-theme 'leuven t))))
 
+(use-package telephone-line
+  :init
+  (setq telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-vc-segment
+                     telephone-line-erc-modified-channels-segment
+                     telephone-line-process-segment))
+          (nil    . (telephone-line-minor-mode-segment
+                     telephone-line-buffer-segment))))
+  (setq telephone-line-rhs
+        '((nil    . (telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment))
+          (evil   . (telephone-line-airline-position-segment))))
+  (add-hook 'after-init-hook #'(lambda()
+                                 (telephone-line-mode 1))))
 ;; (use-package spaceline-config
 ;;   :ensure spaceline
 ;;   :commands spaceline-spacemacs-theme
