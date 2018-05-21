d17d3d19cc496ac374dda8a6d67f60b92cccbe88
lemon0910 <lemon9010@gmail.com>
2018-05-21 09:06:26 +0800

    增加helm的fuzzy find配置

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -6,6 +6,18 @@
          ([escape] . helm-keyboard-quit)
 )  
   :config
+  (setq helm-recentf-fuzzy-match t)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-recentf-fuzzy-match t)
+  (setq helm-buffers-fuzzy-matching t)
+  (setq helm-locate-fuzzy-match t)
+  (setq helm-M-x-fuzzy-match t)
+  (setq helm-semantic-fuzzy-match t)
+  (setq helm-imenu-fuzzy-match t)
+  (setq helm-apropos-fuzzy-match t)
+  (setq helm-lisp-fuzzy-completion t)
+  (setq helm-session-fuzzy-match t)
+  (setq helm-etags-fuzzy-match t)
   (setq helm-mode-fuzzy-match t)
   (setq helm-completion-in-region-fuzzy-match t))
 
