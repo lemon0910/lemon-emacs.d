08ba341f08040fdf260b4582ef21fd91e2d0182d
lemon0910 <lemon9010@gmail.com>
2018-10-05 10:43:03 +0800

    使用doom主题

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -5,7 +5,7 @@
           (const :tag "Emacs-China" emacs-china)
           (const :tag "Tuna" tuna)))
 
-(defcustom my-theme 'srcery
+(defcustom my-theme 'doom
   "Set color theme."
   :type '(choice
           (const :tag "Default theme" default)
